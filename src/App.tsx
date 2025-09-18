01e85d07325e928925f0daa3e89e1b53fa6fd28a
kim10914 <43512783+kim10914@users.noreply.github.com>
2025-09-19 00:36:46 +0900

    feat : 라우트 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router-dom";
+import DummyPage from "./shared/pages/DummyPage";
+import Layout from "./shared/components/Layout";
 
 function App() {
   function isLoggedIn() {
@@ -6,22 +8,25 @@ function App() {
     return !!token;
   }
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={""} />
-        {/* <Route path="/oauth/google/redirect" element={<AuthCallbackPage />} /> */}
+    <Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={""} />
+          {/* <Route path="/oauth/google/redirect" element={<AuthCallbackPage />} /> */}
+          <Route path="/" element={<DummyPage />} />
 
-        <Route
-          path="/"
-          element={isLoggedIn() ? <Outlet /> : <Navigate to="/login" replace />}
-        >
-          {/* 보호 경로 작성 */}
-        </Route>
+          <Route
+            path="/"
+            element={isLoggedIn() ? <Outlet /> : <Navigate to="/login" replace />}
+          >
+            {/* 보호 경로 작성 */}
+          </Route>
 
-        {/* 알 수 없는 경로 처리 */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+          {/* 알 수 없는 경로 처리 */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </Layout>
   )
 }
 
