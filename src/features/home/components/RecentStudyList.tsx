700be2513cc54aeea36e18d6a3fb87bc4ecbc507
kim10914 <43512783+kim10914@users.noreply.github.com>
2025-09-19 08:10:45 +0900

    chore : 스크롤 바 숨기기 옵션 추가

--- src/features/home/components/RecentStudyList.tsx
+++ src/features/home/components/RecentStudyList.tsx
@@ -20,7 +20,7 @@ export default function RecentStudyList({ items }: Props) {
     }
 
     return (
-        <div className=' min-h-60 bg-white rounded-2xl shadow-[0px_4px_4px_0px_#00000040] py-2 px-5'>
+        <div className=' min-h-60 bg-white rounded-2xl shadow-[0px_4px_4px_0px_#00000040] py-2 px-5 overflow-y-auto hide-scrollbar'>
             <p className='text-base font-medium mb-4'>최근 공부로 돌아가기</p>
             <div >
                 {items.map(item => (
