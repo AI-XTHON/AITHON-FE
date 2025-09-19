ee2efe4ca928bce859991402f24858b5a366e079
최지한 <jhlove4061@naver.com>
2025-09-19 09:36:39 +0900

    refactor: Improve routing structure and fix component paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
-import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import DummyPage from "./shared/pages/DummyPage";
 import Layout from "./shared/components/Layout";
 import UserInfoPage from "./features/info";
 import Mypage from "./features/mypage";
 
-function App() {
-  function isLoggedIn() {
-    const token = localStorage.getItem("accessToken");
-    return !!token;
-  }
-  return (
+// Import new pages
+import LoginPage from "./features/login/pages/LoginPage";
+import LoginSuccessPage from "./features/login/pages/LoginSuccessPage";
 
+// Import route components
+import ProtectedRoute from "./shared/components/ProtectedRoute";
+import PublicRoute from "./shared/components/PublicRoute";
+
+function App() {
+  return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          {/* 비 보호 경로 작성 */}
-          <Route path="/login" element={""} />
-          {/* <Route path="/oauth/google/redirect" element={<AuthCallbackPage />} /> */}
-          <Route path="/" element={<DummyPage />} />
-          <Route path="/onboarding/user-info" element={<UserInfoPage mode="onboarding" />} />
-          <Route path="/settings/user-info" element={<UserInfoPage mode="edit" />} />
-          <Route path="/mypage" element={<Mypage />} />
-
-
-          <Route
-            path="/"
-            element={isLoggedIn() ? <Outlet /> : <Navigate to="/login" replace />}
-          >
-            {/* 보호 경로 작성 */}
+          {/* Public routes */}
+          <Route element={<PublicRoute />}>
+            <Route path="/login" element={<LoginPage />} />
           </Route>
 
-          {/* 알 수 없는 경로 처리 */}
+          {/* Protected routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<DummyPage />} />
+            <Route path="/onboarding/user-info" element={<UserInfoPage mode="onboarding" />} />
+            <Route path="/settings/user-info" element={<UserInfoPage mode="edit" />} />
+            <Route path="/mypage" element={<Mypage />} />
+          </Route>
+
+          {/* Standalone routes */}
+          <Route path="/login/success" element={<LoginSuccessPage />} />
+
+          {/* Catch-all for unknown routes */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Layout>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
